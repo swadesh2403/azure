396200fcbc8e5fe6bf1f3f525f271fed30c9c638
swadesh2403 <124901377+swadesh2403@users.noreply.github.com>
2023-11-22 11:13:33 +0000

    changes in main

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 app=FastAPI()
 @app.get("/")
 def default():
-	return{"success":True,"message":This the default page"}
+	return{"success":True,"message":"This the default page"}
 
 @app.get("/home)
 def home():
